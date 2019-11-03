cfc4e0eb3a603f6f1ca08a813e9d47e8e968ded1
Laxmikant <laxmikant.nuchchi@gmail.com>
2019-11-03 19:18:05 +0530

    first commit

---
+++ app/src/main/java/qualitywater/iotivity/com/kotilinrxjavaproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package qualitywater.iotivity.com.kotilinrxjavaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
